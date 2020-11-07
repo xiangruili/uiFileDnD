c13a4ad65adfe2ad1b0c6fcbf869cb8b6a7b7665
xiangruili <xiangrui.li@gmail.com>
2020-11-07 13:10:15 -0500

    Update DnD_uifigure.m
    minor improvements

--- DnD_uifigure.m
+++ DnD_uifigure.m
@@ -10,12 +10,11 @@ function DnD_uifigure(target, dropFcn)
 %    shiftKey: 0 % true if Shift key is down while dropping
 %       names: {'/myPath/myFile'} % cellstr for full file/folder names
 % 
-% Example to drop file/folder into uilistbox:
+% Example to show dropped file/folder onto uilistbox:
 %  target = uilistbox(uifigure, 'Position', [80 100 400 100]);
 %  DnD_uifigure(target, @(o,dat)set(o,'Items',dat.names));
 
 % 201001 Wrote it, by Xiangrui.Li at gmail.com 
-% 201004 Allow to add more target repeatedly
 % 201023 Remove uihtml by using ww.executeJS
 
 narginchk(2, 2);
@@ -33,32 +32,29 @@ end
 
 drawnow; ww = matlab.internal.webwindowmanager.instance.windowList;
 ww = ww(strcmp(matlab.ui.internal.FigureServices.getFigureURL(fh), {ww.URL}));
-if ~ismethod(ww, 'enableDragAndDropAll')
-    error('Matlab R2020b or later needed for file drag and drop');
+try ww.enableDragAndDropAll; % enable file DnD to whole uifigure
+catch, error('Matlab R2020b or later needed for file drag and drop');
 end
-h = uibutton(fh, 'Position', [1 1 0 0], 'Text', 'uiFileDnD_fakeBtn', ...
+h = uibutton(fh, 'Position', [1 1 0 0], 'Text', '4JS2identify_me', ...
     'ButtonPushedFcn', {@drop ww}, 'UserData', {dropFcn target}, ...
     'Tag', 'uiFileDnD', 'Visible', 'off', 'HandleVisibility', 'off'); 
-
-drawnow; ww.executeJS(help([mfilename '>drop']));
-ww.enableDragAndDropAll; % enable file DnD to whole uifigure
+drawnow; ww.executeJS(sprintf(help([mfilename '>drop']), h.Text));
 ww.FileDragDropCallback = {@dragEnter h};
 
 %% fired when drag enters uifigure
 function dragEnter(ww, names, h)
-pos = '';
 for i = size(h.UserData,1):-1:1 %  redo in case pos changed or resized
-    p = getpixelposition(h.UserData{i,2}, 1);
-    if h.UserData{i,2}.Type == "figure", p(1:2) = 1; end
-    pos = sprintf('[%g,%g,%g,%g],%s', p, pos);
+    p{i} = round(getpixelposition(h.UserData{i,2}, 1));
+    if h.UserData{i,2}.Type == "figure", p{i}(1:2) = 1; end
 end
-ww.executeJS(['uiFileDnD.rects=[' pos '];']);
-setappdata(h, 'names', cellstr(names)); % store file names
+ww.executeJS(['uiFileDnD.rects=' jsonencode(p)]);
+h.Text = cellstr(names); % store file names
 
 %% fired by javascript fake button press in ondrop
 % // help text is actually javascript. Touch with care!
 function drop(h, ~, ww)
-% uiFileDnD = {rects: [], index: 0, ctrlKey: false, shiftKey: false};
+% uiFileDnD = {rects: [], index: 0,
+%     button: dojo.query(".mwPushButton").find(b => b.textContent==="%s")};
 % document.ondragenter = (e) => { // prevent default before firing ondragover
 %   e.dataTransfer.dropEffect = "none"; 
 %   return false;
@@ -76,19 +72,14 @@ function drop(h, ~, ww)
 %   e.dataTransfer.dropEffect = "none"; // disable drop
 % };
 % document.ondrop = (e) => {
-%   uiFileDnD.ctrlKey = e.ctrlKey;
-%   uiFileDnD.shiftKey = e.shiftKey;
-%   fakeButton().click(); // fire Matlab drop()
-%   return false;
-% };
-% function fakeButton() {
-%   for (let b of document.getElementsByClassName("mwPushButton")) {
-%     if (b.textContent == "uiFileDnD_fakeBtn") return b;}
+%   e.returnValue = false;
+%   uiFileDnD.data = {ctrlKey: e.ctrlKey, shiftKey: e.shiftKey};
+%   uiFileDnD.button.click(); // fire Matlab callback
 % };
 
-dat = jsondecode(ww.executeJS('uiFileDnD'));
-dat.names = getappdata(h, 'names');
-args = [h.UserData(dat.index+1,:) rmfield(dat, {'rects' 'index'})];
+dat = jsondecode(ww.executeJS('uiFileDnD.data'));
+dat.names = h.Text;
+args = [h.UserData(jsondecode(ww.executeJS('uiFileDnD.index'))+1,:) dat];
 if iscell(args{1}), args = [args{1}(1) args(2:3) args{1}(2:end)]; end
 feval(args{:});
 %%
