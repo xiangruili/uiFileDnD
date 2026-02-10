2dd44257eac5ad64563e091d35ea3808aff924bb
xiangruili <xiangrui.li@gmail.com>
2026-02-09 22:06:13 -0500

    Update java_dnd.m
    use WindowsKeypressedFcn so shiftKey works too

--- java_dnd.m
+++ java_dnd.m
@@ -4,6 +4,7 @@ function java_dnd(target, dropFcn)
 % 170421 Xiangrui Li adapted from dndcontrol class by Maarten van der Seijs:
 %   https://www.mathworks.com/matlabcentral/fileexchange/53511
 % 260201 include findjobj_fast(), so input is figure component
+% 260209 use fh.WindowKeyPressFcn to catch ctrlKey/shiftKey
 
 % Required: MLDropTarget.class under the same folder
 
@@ -23,52 +24,34 @@ if ~exist('MLDropTarget', 'class')
 end
 
 dropTarget = handle(javaObjectEDT('MLDropTarget'), 'CallbackProperties');
-set(dropTarget, 'DragEnterCallback', @DragEnterCallback, ...
-                'DragExitCallback', @DragExitCallback, ...
-                'DropCallback', {@DropCallback target dropFcn});
+set(dropTarget, 'DropCallback', {@DropCallback target dropFcn});
 jObj = handle(findjobj_fast(target), 'CallbackProperties');
 jObj.setDropTarget(dropTarget);
 %%
 
-function DropCallback(jSource, jEvent, target, dropFcn)
-setComplete = onCleanup(@()jEvent.dropComplete(true));
-% Following DropAction is for ~jEvent.isLocalTransfer, such as dropping file.
-% For LocalTransfer, Linux seems consistent with other OS.
-% DropAction: Neither ctrl nor shift Dn, PC/MAC 2, Linux 1
-% All OS: ctrlDn 1, shiftDn 2, both Dn 1073741824 (2^30)
-if ispc || ismac
-    evt.ctrlKey = jEvent.getDropAction() ~= 2;
-else % fails to report CtrlDn if user releases shift between DragEnter and Drop
-    evt.ctrlKey = bitget(jEvent.getDropAction,31)>0; % ACTION_LINK 1<<30
-    java.awt.Robot().keyRelease(16); % shift up
+function DropCallback(jSource, ~, target, dropFcn)
+evt.names = cellstr(char(jSource.getTransferData()));
+if strncmp(evt.names, 'file://', 7) % files identified as string
+    evt.names = regexp(evt.names, '(?<=file://).*?(?=\r?\n)', 'match')';
 end
-% evt.Location = [jEvent.getLocation.x jEvent.getLocation.y]; % top-left [0 0]
-if jSource.getDropType() == 1 % String dropped
-    evt.DropType = 'string';
-    evt.names = char(jSource.getTransferData());
-    if strncmp(evt.Data, 'file://', 7) % files identified as string
-        evt.DropType = 'file';
-        evt.names = regexp(evt.names, '(?<=file://).*?(?=\r?\n)', 'match')';
-    end
-elseif jSource.getDropType() == 2 % file(s) dropped
-    evt.DropType = 'file';
-    evt.names = cell(jSource.getTransferData());
-else, return; % No success
-end
+
+% Try to detect control and shift key during drop 
+fh = ancestor(target, 'figure');
+keyFcn = fh.WindowKeyPressFcn;
+restoreKeyFcn = onCleanup(@()set(fh,'WindowKeyPressFcn',keyFcn));
+fh.WindowKeyPressFcn = @(o,e)setappdata(fh,'Modifiers',e.Modifier);
+figure(fh); drawnow;
+bot = java.awt.Robot();
+k = java.awt.event.KeyEvent.VK_CAPS_LOCK; % no harm key
+bot.keyPress(k); bot.keyRelease(k); bot.keyPress(k); pause(0.05); bot.keyRelease(k);
+dat = getappdata(fh, 'Modifiers');
+if ~iscell(dat), dat = {}; end % in case Robot() fails
+evt.ctrlKey = contains('control', dat);
+evt.shiftKey = contains('shift', dat);
 
 if iscell(dropFcn), feval(dropFcn{1}, target, evt, dropFcn{2:end});
 else, feval(dropFcn, target, evt);
 end
-%%
-
-function DragEnterCallback(~, jEvent)
-try jEvent.acceptDrag(1); catch, return; end % ACTION_COPY
-if ~ispc && ~ismac, java.awt.Robot().keyPress(16); end % shift down
-%%
-
-function DragExitCallback(~, jEvent)
-if ~ispc && ~ismac, java.awt.Robot().keyRelease(16); end % shift up
-try jEvent.rejectDrag(1); catch, end % ACTION_COPY
 %%
 
 % Rest code from Yair Altman (2026). findjobj - find java handles of Matlab graphic objects
