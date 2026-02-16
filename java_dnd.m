1d30ae0dde95eafb1f7b82b53f5818829e2b29a4
xiangruili <xiangrui.li@gmail.com>
2026-02-16 12:42:31 -0500

    Update java_dnd.m

--- java_dnd.m
+++ java_dnd.m
@@ -30,11 +30,6 @@ jObj.setDropTarget(dropTarget);
 %%
 
 function DropCallback(jSource, ~, target, dropFcn)
-evt.names = cellstr(char(jSource.getTransferData()));
-if strncmp(evt.names, 'file://', 7) % files identified as string
-    evt.names = regexp(evt.names, '(?<=file://).*?(?=\r?\n)', 'match')';
-end
-
 % Try to detect control and shift key during drop 
 fh = ancestor(target, 'figure');
 keyFcn = fh.WindowKeyPressFcn;
@@ -49,87 +44,77 @@ if ~iscell(dat), dat = {}; end % in case Robot() fails
 evt.ctrlKey = contains('control', dat);
 evt.shiftKey = contains('shift', dat);
 
+evt.names = cellstr(char(jSource.getTransferData()));
+if strncmp(evt.names, 'file://', 7) % files identified as string
+    evt.names = regexp(evt.names, '(?<=file://).*?(?=\r?\n)', 'match')';
+end
+
 if iscell(dropFcn), feval(dropFcn{1}, target, evt, dropFcn{2:end});
 else, feval(dropFcn, target, evt);
 end
 %%
 
-% Rest code from Yair Altman (2026). findjobj - find java handles of Matlab graphic objects
+% findjobj_fast() from Yair Altman (2026). findjobj - find java handles of Matlab graphic objects
 % https://www.mathworks.com/matlabcentral/fileexchange/14317-findjobj-find-java-handles-of-matlab-graphic-objects
-function jControl = findjobj_fast(hControl, jContainer) %#ok<*JAVFM,*JAPIMATHWORKS>
-    if double(get(hControl,'Parent'))~=0  % avoid below for figure handles
-        try jControl = hControl.getTable; return, catch, end  % fast bail-out for old uitables
-        try jControl = hControl.JavaFrame.getGUIDEView; return, catch, end  % bail-out for HG2 matlab.ui.container.Panel
-    end
-    oldWarn = warning;
-    warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
-    warning('off','MATLAB:ui:javaframe:PropertyToBeRemoved');
-    try oc = onCleanup(@()warning(oldWarn)); catch, end % Gracefully restore warnings on return (suggested by T. Carpenter 2021-08-17)
-    if nargin < 2 || isempty(jContainer)
-        % Use a HG2 matlab.ui.container.Panel jContainer if the control's parent is a uipanel
-        try
-            hParent = get(hControl,'Parent');
-        catch
-            % Probably indicates an invalid/deleted/empty handle
-            jControl = [];
-            return
-        end
-        try jContainer = hParent.JavaFrame.getGUIDEView; catch, jContainer = []; end
-    end
-    if isempty(jContainer)
-        if isequal(double(hControl),0)  % root handle => return jDesktop
-            try
-                jControl = com.mathworks.mde.desk.MLDesktop.getInstance;
-                if isempty(jControl), error('retry'), end
-            catch
-                jControl = com.mathworks.mlservices.MatlabDesktopServices.getDesktop;
-            end
-            return
-        end
-        hFig = ancestor(hControl,'figure');
-        jf = get(hFig, 'JavaFrame');
-        if isequal(hFig,hControl) % speedup suggested by T. Carpenter 2021-08-17
-            jControl = jf;
-            return
-        end
-        jContainer = jf.getFigurePanelContainer.getComponent(0);
-    end
-    warning(oldWarn);
+function jControl = findjobj_fast(hControl) %#ok<*JAVFM,*JAPIMATHWORKS>
+if double(get(hControl,'Parent'))~=0  % avoid below for figure handles
+    try jControl = hControl.getTable; return, catch, end  % fast bail-out for old uitables
+    try jControl = hControl.JavaFrame.getGUIDEView; return, catch, end  % bail-out for HG2 matlab.ui.container.Panel
+end
+oldWarn = warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
+warning('off','MATLAB:ui:javaframe:PropertyToBeRemoved');
+restoreWarn = onCleanup(@()warning(oldWarn));
+% Use a HG2 matlab.ui.container.Panel jContainer if the control's parent is a uipanel
+try
+    hParent = get(hControl,'Parent');
+catch
+    % Probably indicates an invalid/deleted/empty handle
     jControl = [];
-    counter = 20;  % 2018-09-21 speedup (100 x 0.001 => 20 x 0.005) - Martin Lehmann suggestion on FEX 2016-06-07
-    specialTooltipStr = '!@#$%^&*';
-    try  % Fix for R2018b suggested by Eddie (FEX comment 2018-09-19)
-        tooltipPropName = 'TooltipString';
-        oldTooltip = get(hControl,tooltipPropName);
-        set(hControl,tooltipPropName,specialTooltipStr);
-    catch
-        tooltipPropName = 'Tooltip';
-        oldTooltip = get(hControl,tooltipPropName);
-        set(hControl,tooltipPropName,specialTooltipStr);
+    return
+end
+try jContainer = hParent.JavaFrame.getGUIDEView; catch, jContainer = []; end
+if isempty(jContainer)
+    hFig = ancestor(hControl,'figure');
+    jf = get(hFig, 'JavaFrame');
+    if isequal(hFig,hControl) % speedup suggested by T. Carpenter 2021-08-17
+        jControl = jf.getAxisComponent;
+        return
     end
-    while isempty(jControl) && counter>0
-        counter = counter - 1;
-        pause(0.005);
-        jControl = findTooltipIn(jContainer, specialTooltipStr);
-    end
-    set(hControl,tooltipPropName,oldTooltip);
-    try jControl.setToolTipText(oldTooltip); catch, end
-    try jControl = jControl.getParent.getView.getParent.getParent; catch, end  % return JScrollPane if exists
+    jContainer = jf.getFigurePanelContainer.getComponent(0);
+end
+jControl = [];
+specialTooltipStr = '!@#$%^&*';
+try  % Fix for R2018b suggested by Eddie (FEX comment 2018-09-19)
+    tooltipPropName = 'TooltipString';
+    oldTooltip = get(hControl,tooltipPropName);
+    set(hControl,tooltipPropName,specialTooltipStr);
+catch
+    tooltipPropName = 'Tooltip';
+    oldTooltip = get(hControl,tooltipPropName);
+    set(hControl,tooltipPropName,specialTooltipStr);
+end
+restoreTooltip = onCleanup(@()set(hControl,tooltipPropName,oldTooltip));
+for counter = 1:3
+    jControl = findTooltipIn(jContainer, specialTooltipStr);
+    if ~isempty(jControl), break; else, pause(0.017); end
+end
+set(hControl,tooltipPropName,oldTooltip);
+try jControl.setToolTipText(oldTooltip); catch, end
+try jControl = jControl.getParent.getView.getParent.getParent; catch, end  % return JScrollPane if exists
 
 function jControl = findTooltipIn(jContainer, specialTooltipStr)
-    try
-        jControl = [];  % Fix suggested by H. Koch 11/4/2017
-        tooltipStr = jContainer.getToolTipText;
-        %if strcmp(char(tooltipStr),specialTooltipStr)
-        if ~isempty(tooltipStr) && tooltipStr.startsWith(specialTooltipStr)  % a bit faster
-            jControl = jContainer;
-        else
-            for idx = 1 : jContainer.getComponentCount
-                jControl = findTooltipIn(jContainer.getComponent(idx-1), specialTooltipStr);
-                if ~isempty(jControl), return; end
-            end
+try
+    jControl = [];  % Fix suggested by H. Koch 11/4/2017
+    tooltipStr = jContainer.getToolTipText;
+    if ~isempty(tooltipStr) && tooltipStr.startsWith(specialTooltipStr)  % a bit faster
+        jControl = jContainer;
+    else
+        for idx = 1 : jContainer.getComponentCount
+            jControl = findTooltipIn(jContainer.getComponent(idx-1), specialTooltipStr);
+            if ~isempty(jControl), return; end
         end
-    catch
-        % ignore
     end
+catch
+    % ignore
+end
 %%
