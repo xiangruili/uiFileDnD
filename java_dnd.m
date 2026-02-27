a6474f87d259729e17db26ac67463cb8565481a1
xiangruili <xiangrui.li@gmail.com>
2026-02-27 13:41:26 -0500

    Update java_dnd.m
    work for uicontrol Style=edit

--- java_dnd.m
+++ java_dnd.m
@@ -1,99 +1,99 @@
-function java_dnd(target, dropFcn)
-% Set Matlab dropFcn for a figure object. Called by uiFileDnD.m.
+function java_dnd(obj, dropFcn)
+% Set Matlab dropFcn for a figure or its object. Called by uiFileDnD.m.
 
 % 170421 Xiangrui Li adapted from dndcontrol class by Maarten van der Seijs:
 %   https://www.mathworks.com/matlabcentral/fileexchange/53511
-% 260201 include findjobj_fast(), so input is figure component
 % 260209 use fh.WindowKeyPressFcn to catch ctrlKey/shiftKey
-% 260225 adopt similar approach to uiFileDnD.m: findjobj not needed
+% 260225 adopt similar approach to uiFileDnD.m, so work for axes
+% 260227 also work for uicontrol Style=edit
 
 % Required: MLDropTarget.class on javapath or under the same folder
+% Tricks to make this work:
+%  Set figure jFrame as Component for MLDropTarget, so the figure accepts drop.
+%  Store drop-accepting object and dropFcn into figure appdata.
+%  In DragEnter, get obj position (take care of java cords scale issue), also
+%   fake 'edit' as 'pushbutton' to enable DragOver.
+%  In DragOver, acceptDrag only if mouse over drop components and store the
+%   component index by Positio. Slow down DragOver to avoid Matlab crash.
+%  In Drop, get file names from MLDropTarget, also use a temporary keyFcn to
+%   detect control and shift key during drop. Then fire dropFcn.
 
-fh = ancestor(target, 'figure');
-targets = getappdata(fh, 'uiFileDnD_target');
-if ~isempty(targets)
-    targets(end+1,:) = {dropFcn target};
-    setappdata(fh, 'uiFileDnD_target', targets);
+fh = ancestor(obj, 'figure');
+FncObj = getappdata(fh, 'uiFileDnD_FncObj');
+if ~isempty(FncObj)
+    ind = find([FncObj{:,2}]==obj, 1, 'last');
+    if isempty(ind), FncObj(end+1,:) = {dropFcn obj};
+    else, FncObj{ind,1} = dropFcn;
+    end
+    setappdata(fh, 'uiFileDnD_FncObj', FncObj);
     return;
 end
-
-if ~exist('MLDropTarget', 'class')
-    pth = fileparts(mfilename('fullpath'));
-    javaaddpath(pth); % dynamic for this session
-    fid = fopen(fullfile(prefdir, 'javaclasspath.txt'), 'a+');
-    if fid>0 % static path for later sessions: work for 2013+?
-        autoClose = onCleanup(@() fclose(fid));
-        fseek(fid, 0, 'bof');
-        classpth = fread(fid, inf, '*char')';
-        if isempty(strfind(classpth, pth)) %#ok<*STREMP> % avoid multiple write
-            fseek(fid, 0, 'bof');
-            fprintf(fid, '%s\n', pth);
-        end
-    end
-end
-
-dropTarget = handle(javaObjectEDT('MLDropTarget'), 'CallbackProperties');
-set(dropTarget, 'DragOverCallback', {@DragOverCallback fh});
-set(dropTarget, 'DropCallback', {@DropCallback fh});
 
 oldWarn = warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
 warning('off','MATLAB:ui:javaframe:PropertyToBeRemoved');
 resetWarn = onCleanup(@()warning(oldWarn));
-jObj = handle(fh.JavaFrame.getAxisComponent, 'CallbackProperties'); %#ok
-jObj.setDropTarget(dropTarget);
-setappdata(fh, 'uiFileDnD_target', {dropFcn target});
+jFrame = handle(fh.JavaFrame.getAxisComponent, 'CallbackProperties'); %#ok
 
-% java dimension does not take care of display scale
-sz = getpixelposition(fh);
-dispScale = [jObj.getWidth jObj.getHeight] ./ sz(3:4);
-setappdata(fh, 'uiFileDnD_dispScale', round(dispScale*4)/4);
-%%
+if ~exist('MLDropTarget', 'class'), javaaddpath(fileparts(mfilename('fullpath'))); end
+dropTarget = handle(javaObjectEDT('MLDropTarget'), 'CallbackProperties');
+set(dropTarget, 'Component', jFrame, 'DragEnterCallback', {@Callback fh jFrame}, ...
+    'DragOverCallback', @Callback, 'DropCallback', @Callback, 'DragExitCallback', @Callback);
+setappdata(fh, 'uiFileDnD_FncObj', {dropFcn obj});
 
-function DragOverCallback(~, jEvent, fh)
-persistent lastOver
-if isempty(lastOver), lastOver = datetime; end
-if datetime-lastOver<seconds(0.02), return; end
-lastOver = datetime;
-dispScale = getappdata(fh, 'uiFileDnD_dispScale');
-x = jEvent.getLocation.getX/dispScale(1);
-y = fh.Position(4) - jEvent.getLocation.getY/dispScale(2);
-targets = getappdata(fh, 'uiFileDnD_target');
-for i = size(targets,1):-1:1
-    p = getpixelposition(targets{i,2}, true);
-    if all(p==0), p = getpixelposition(targets{i,2}.Parent, true);
-    elseif targets{i,2}.Type == "figure", p(1:2) = 1;
+%% use signle callback to share data via persistent
+function Callback(dropTarget, jEvent, varargin)
+persistent R fh dispScale lastOver index FncObj hEdit
+evt = jEvent.getClass.getName;
+if nargin>2 % DragEnter: evt same as DragOver
+    fh = varargin{1};
+    pos = getpixelposition(fh);
+    dispScale = round([varargin{2}.getWidth varargin{2}.getHeight]./pos(3:4)*4)/4;
+    hEdit = []; R = [];
+    FncObj = getappdata(fh, 'uiFileDnD_FncObj');
+    FncObj = FncObj(isvalid([FncObj{:,2}]), :); % ignore invalid components
+    for i = size(FncObj,1):-1:1
+        h = FncObj{i,2};
+        R(i,:) = getpixelposition(h, true);
+        if all(R(i,:)==0), R(i,:) = getpixelposition(h.Parent, true);
+        elseif h.Type == "figure", R(i,1:2) = 1;
+        elseif h.Type=="uicontrol" && h.Style=="edit" && h.Visible=="on"
+            hEdit(end+1) = h; %#ok change back to 'edit' after Drop/Exit
+            h.Style = 'pushbutton';
+        end
     end
-    if x>p(1) && x<p(1)+p(3) && y>p(2) && y<p(2)+p(4)
-        jEvent.acceptDrag(java.awt.dnd.DnDConstants.ACTION_COPY_OR_MOVE);
-        setappdata(fh, 'uiFileDnD_index', i);
-        return;
+    R(:,2) = pos(4) - R(:,2) - R(:,4); % from top for java coords
+    R(:,3:4) = R(:,3:4) + R(:,1:2); % rect
+    return;
+elseif endsWith(evt, 'DragEvent') % DragOver
+    if isempty(lastOver), lastOver = datetime; end
+    if datetime-lastOver<seconds(0.02), return; end
+    lastOver = datetime;
+    x = jEvent.getLocation.getX / dispScale(1);
+    y = jEvent.getLocation.getY / dispScale(2);
+    index = find(x>R(:,1) & x<R(:,3) & y>R(:,2) & y<R(:,4), 1, "last");
+    if isempty(index), jEvent.rejectDrag(); % reject if outside obj
+    else, jEvent.acceptDrag(3) % ACTION_COPY_OR_MOVE
     end
+    return;
+elseif endsWith(evt, 'DropEvent') % Drop
+    keyFcn = fh.WindowKeyPressFcn;
+    resetFcn = onCleanup(@()set(fh,'WindowKeyPressFcn',keyFcn));
+    fh.WindowKeyPressFcn = @(o,e)assignin('caller','Modifier',e.Modifier);
+    figure(fh); drawnow;
+    bot = java.awt.Robot();
+    k = java.awt.event.KeyEvent.VK_CAPS_LOCK; % no harm key
+    bot.keyPress(k); bot.keyRelease(k); bot.keyPress(k); pause(0.05); bot.keyRelease(k);
+    if ~exist('Modifier', 'var'), Modifier = {}; end % in case Robot() fails
+    dat.ctrlKey = contains('control', Modifier);
+    dat.shiftKey = contains('shift', Modifier);
+
+    dat.names = cellstr(char(dropTarget.getTransferData()));
+    if strncmp(dat.names, 'file://', 7) % files identified as string
+        dat.names = regexp(dat.names, '(?<=file://).*?(?=\r?\n)', 'match')';
+    end
+
+    args = [FncObj(index,:) dat];
+    if iscell(args{1}), args = [args{1}(1) args(2:3) args{1}(2:end)]; end
+    feval(args{:});
 end
-jEvent.rejectDrag();
-%%
-
-function DropCallback(jSource, ~, fh)
-% Try to detect control and shift key during drop 
-keyFcn = fh.WindowKeyPressFcn;
-resetFcn = onCleanup(@()set(fh,'WindowKeyPressFcn',keyFcn));
-fh.WindowKeyPressFcn = @(o,e)setappdata(o,'uiFileDnD_Modifier',e.Modifier);
-figure(fh); drawnow;
-bot = java.awt.Robot();
-k = java.awt.event.KeyEvent.VK_CAPS_LOCK; % no harm key
-bot.keyPress(k); bot.keyRelease(k); bot.keyPress(k); pause(0.05); bot.keyRelease(k);
-Modifier = getappdata(fh, 'uiFileDnD_Modifier');
-if ~iscell(Modifier), Modifier = {}; end % in case Robot() fails
-evt.ctrlKey = contains('control', Modifier);
-evt.shiftKey = contains('shift', Modifier);
-
-evt.names = cellstr(char(jSource.getTransferData()));
-if strncmp(evt.names, 'file://', 7) % files identified as string
-    evt.names = regexp(evt.names, '(?<=file://).*?(?=\r?\n)', 'match')';
-end
-
-targets = getappdata(fh, 'uiFileDnD_target');
-args = [targets(getappdata(fh,'uiFileDnD_index'),:) evt];
-if iscell(args{1}), args = [args{1}(1) args(2:3) args{1}(2:end)]; end
-feval(args{:});
-
-%%
+set(hEdit, 'Style', 'edit'); % DragExit or Drop
